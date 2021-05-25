57fd0ceef1dc8a8bdb05bfb093d7db8db08d4aef
Karlsson <JKARL163@volvocars.com>
2021-05-25 11:45:44 +0200

    updating some stuff

--- source/main.py
+++ source/main.py
@@ -1,4 +1,9 @@
 from source.routes import signal_interpreter_app
 
-if __name__ == '__main__':
+
+def main():
     signal_interpreter_app.run()
+
+
+if __name__ == '__main__':
+    main()
--- source/routes.py
+++ source/routes.py
@@ -3,9 +3,8 @@ from flask import Flask, request
 
 signal_interpreter_app = Flask(__name__)
 
+
 @signal_interpreter_app.route("/", methods=["POST"])
 def mirror_data():
     data = request.get_json()
     return data
-
-
