07f5de4f776182627a4945cd387eab283f0dcead
Karlsson <JKARL163@volvocars.com>
2021-05-25 11:21:42 +0200

    files

--- source/routes.py
+++ source/routes.py
@@ -1,12 +1,11 @@
 # routes.py
-from flask import Flask
+from flask import Flask, request
 
-my_app = Flask(__name__)
+signal_interpreter_app = Flask(__name__)
+
+@signal_interpreter_app.route("/", methods=["POST"])
+def mirror_data():
+    data = request.get_json()
+    return data
 
 
-@my_app.route("/", methods=["GET"])
-def hello():
-    return "Hello world!"
-
-
-my_app.run()
