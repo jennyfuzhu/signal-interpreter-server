ad92264b1f711f6431e0df660a4b7a95557eaa7b
Karlsson <JKARL163@volvocars.com>
2021-06-09 20:13:19 +0200

    test_json_parser done

--- source/json_parser.py
+++ source/json_parser.py
@@ -9,15 +9,13 @@ class JsonParser:
         # open the json file
         # load the json file and save it to self.data
         with open(file_path, 'r') as my_file:
-            data = my_file.read()
-        self.data = json.loads(data)
-        return data
+            self.data = json.load(my_file)
+            print(self.data)
 
     def get_signal_title(self, identifier):
         # loop through all services in self.data
         # if the service ID is the identifier, return the title
-        for names in self.data["services"]:
-            if str(identifier) == names["id"]:
-                signal_title = names["title"]
-        return signal_title
-
+        for names in self.data['services']:
+            if str(identifier) == names['id']:
+                signal_title = names['title']
+                return signal_title
--- source/test_json_parser.py
+++ source/test_json_parser.py
@@ -1,29 +1,19 @@
-import os
-import unittest
+# test_json_parser.py
 from unittest.mock import patch, mock_open
 from source.json_parser import JsonParser
-import json
 
-json_parser_class = JsonParser()
-json_parser_class.data = {"services": [{"title": "ECU Reset", "id": "11"}]}
-"""
+input_json = '{"json": "Json Jsonsson"}'
+output_json = {"json": "Json Jsonsson"}
 
 
-"""
-class TestJsonParser(unittest.TestCase):
+def test_load_file():
+    with patch("builtins.open", mock_open(read_data=input_json)):
+        json_parser_class = JsonParser()
+        json_parser_class.load_file('random/path')
+        assert json_parser_class.data == output_json
 
-    def test_load_file(self, tmp):
-        with patch("builtins.open", mock_open(read_data="Hello")):
-            tempt_data = {"services": [{"title": "ECU Reset", "id": "11"}]}
-            file_path = os.path.join(tmp, "temp_json.json")
 
-            with open(file_path, 'w') as json_file:
-                json.dump(tempt_data, json_file)
-
-            json_parser_class.load_file(file_path)
-            assert isinstance(json_parser_class.data, dict)
-
-            assert json_parser_class.data == tempt_data
-
-    def test_get_signal_title(self):
-        assert json_parser_class.get_signal_title(11) == "ECU reset"
+def test_get_signal_title():
+    json_parser_class = JsonParser()
+    json_parser_class.data = {'services': [{'title': 'ECU Reset', 'id': '11'}]}
+    assert json_parser_class.get_signal_title('11') == "ECU Reset"
