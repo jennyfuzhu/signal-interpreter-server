9966007ee0d557a564918fc806abdeb07dcb25e2
Karlsson <JKARL163@volvocars.com>
2021-07-06 20:50:14 +0200

    fixed many things

--- tests/unit/test_factory.py
+++ tests/unit/test_factory.py
@@ -4,79 +4,36 @@ import logging
 import pytest
 from src.json_parser import JsonParser
 from src.xml_parser import XmlParser
+from src.parser_factory import ParseFactory
+
+log = logging.getLogger(__name__)
+parse_factory = ParseFactory()
+
+class MockParser:
+    """Mock parser class"""
 
 
-logger = logging.getLogger(__name__)
+def test_set_signal_database_format():
+    """Test set signal database format unit test"""
+    log.debug("Entering %s unit test" % test_set_signal_database_format.__name__)
+    parse_factory.set_signal_database_format("EXTENSION")
+    assert parse_factory._signal_database_format == "EXTENSION"
+    log.debug("Existing %s unit test" % test_set_signal_database_format.__name__)
 
 
-@pytest.mark.parametrize("data_format", [
-    ".json",
-    ".xml",
-])
-def test_set_signal_database_format(parse_factory_instance, data_format):
-    """
-    Test set format method.
-    :param parse_factory_instance:
-    :param data_format:
-    :param parser_class:
-    :return:
-    """
-    logger.debug("Start of %s function test log.", test_set_signal_database_format.__name__)
-    parse_factory_instance.set_signal_database_format(data_format)
-    assert parse_factory_instance._signal_database_format == data_format
-    logger.debug("End of %s function test log.", test_set_signal_database_format.__name__)
+def test_register_format():
+    "Test register format unit test"
+    log.debug("Entering %s unit test:" % test_register_format.__name__)
+    parse_factory.register_format("FORMAT", MockParser)
+    assert isinstance(parse_factory._parsers["FORMAT"], MockParser)
+    log.debug("Exiting %s unit test" % test_register_format.__name__)
 
 
-@pytest.mark.parametrize("data_format, parser_class", [
-    (".json", JsonParser),
-    (".xml", XmlParser)
-])
-def test_register_format(parse_factory_instance, data_format, parser_class):
-    """
-    Test register format method.
-    :param parse_factory_instance:
-    :param data_format:
-    :param parser_class:
-    :return:
-    """
-    logger.debug("Start of %s function test log.", test_register_format.__name__)
-    parse_factory_instance.register_format(data_format, parser_class)
-    assert isinstance(parse_factory_instance._parsers[data_format], parser_class)  # different instances
-    logger.debug("End of %s function test log.", test_register_format.__name__)
+def test_get_parser():
+    """Test get parser unit test"""
+    log.debug("Entering %s unit test:" % test_get_parser.__name__)
+    parse_factory._parsers["EXTENSION"] = MockParser
+    parse_factory._signal_database_format = "EXTENSION"
+    assert parse_factory.get_parser() == MockParser
+    log.debug("Exiting %s unit test:" % test_get_parser.__name__)
 
-
-@pytest.mark.parametrize("data_format, parser_class", [
-    (".json", JsonParser),
-    (".xml", XmlParser)
-])
-def test_get_parser(parse_factory_instance, data_format, parser_class):
-    """
-    Testing get parser.
-    :param mock_get_signal_title_from_id:
-    :param sig:
-    :return:
-    """
-    logger.debug("Start of %s function test log.", test_get_parser.__name__)
-    # parse_factory_instance.register_format(data_format, parser_class)
-    # parse_factory_instance.set_signal_database_format(data_format)
-    parse_factory_instance._parsers[data_format] = parser_class()  # why mock function when we can set value
-    parse_factory_instance._signal_database_format = data_format  # why mock function when we can set value
-    assert isinstance(parse_factory_instance.get_parser(), parser_class)  # different instances
-    logger.debug("End of %s function test log.", test_get_parser.__name__)
-
-
-@pytest.mark.parametrize("data_format", [
-    ".lol",
-])
-def test_get_parser_exception(parse_factory_instance, data_format):
-    """
-    Testing get parser exception.
-    :param parse_factory_instance:
-    :param data_format:
-    :return:
-    """
-    logger.debug("Start of %s function test log.", test_get_parser.__name__)
-    with pytest.raises(ValueError):
-        parse_factory_instance._signal_database_format = data_format  # why mock function when we can set value
-        parse_factory_instance.get_parser()
-    logger.debug("End of %s function test log.", test_get_parser.__name__)
